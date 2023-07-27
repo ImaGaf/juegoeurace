edf2630914e9ad6ee6e0e77366b3104fd3a1295c
Oswaldo <oswaldoj.tipan@gmail.com>
2023-07-27 09:51:29 -0500

    ass

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -7,7 +7,7 @@ const Score = ({points, tiempoInicial}) => {
     const time = ((tiempoFinal - tiempoInicial.tiempoInicial) / 1000).toFixed(2);
     return (
         <div>
-            <h1>Felicidades tu Score es: {points}  y tu tiempo fue de: {time} segundos</h1>
+            <h1>Felicidades tu Score es: {points} puntos<br/> <br/> y tu tiempo fue de: {time} segundos</h1>
         </div>
     );
 };
